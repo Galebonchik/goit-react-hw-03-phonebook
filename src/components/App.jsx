29736edc4cfb3ffa46bc228bdb031eea30e5f1f1
Tetiana Savinykh <117289657+Galebonchik@users.noreply.github.com>
2023-06-04 21:30:53 +0200

    solved task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,22 @@ import { TelephoneBook, Title } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+  };
+
+  componentDidMount = () => {
+    const contacts = JSON.parse(localStorage.getItem(this.contacts));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  };
+
+  componentDidUpdate = (_, prevState) => {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem(this.contacts, JSON.stringify(contacts));
+    }
   };
 
   addContact = (name, number) => {
